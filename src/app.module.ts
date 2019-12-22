144028bf667d0be4174347d0f34dce6599b4f5e3
jinhohong <tpdleps@gmail.com>
2019-12-23 02:59:40 +0900

    feat: 🎸 add Recipe module on appModules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,30 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { GraphQLModule } from "@nestjs/graphql";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { Post } from "./test.entity";
+import { RecipesModule } from "./recipes/recipes.module";
+import { Recipe } from "./recipes/model/recipe.entity";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: "postgres",
       host: process.env.DB_HOST || "localhost",
-      port: Number(process.env.DB_PORT!) || 3306,
-      username: process.env.DB_USERNAME! || "root",
+      port: Number(process.env.DB_PORT!) || 5432,
+      username: process.env.DB_USERNAME! || "hongjinho",
       password: process.env.DB_PASSWORD! || "password",
-      database: process.env.DB_DATABASE! || "root",
-      entities: [Post],
+      database: process.env.DB_DATABASE! || "mup",
+      entities: [Recipe],
       synchronize: true,
     }),
+    GraphQLModule.forRoot({
+      installSubscriptionHandlers: true,
+      autoSchemaFile: "schema.gql",
+      include: [RecipesModule],
+    }),
+    RecipesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
