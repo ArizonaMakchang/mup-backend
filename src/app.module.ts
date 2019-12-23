b5e689d995c9ae6fdea4463e5aff67e56ced15e2
jinho hong <tpdleps@gmail.com>
2019-12-23 12:30:02 +0900

    feat: 🎸 add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { GraphQLModule } from "@nestjs/graphql";
+import { ConfigModule } from "@nestjs/config";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
@@ -9,13 +10,14 @@ import { Recipe } from "./recipes/model/recipe.entity";
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: process.env.DB_HOST || "localhost",
-      port: Number(process.env.DB_PORT!) || 5432,
-      username: process.env.DB_USERNAME! || "hongjinho",
-      password: process.env.DB_PASSWORD! || "password",
-      database: process.env.DB_DATABASE! || "mup",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT!),
+      username: process.env.DB_USERNAME!,
+      password: process.env.DB_PASSWORD!,
+      database: process.env.DB_DATABASE!,
       entities: [Recipe],
       synchronize: true,
     }),
